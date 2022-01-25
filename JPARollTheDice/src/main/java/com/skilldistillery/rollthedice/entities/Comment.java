a071b137668f671c3185f28490d30147023fbe85
Max McGregor <maxmcgregor1@gmail.com>
2022-01-25 17:02:10 -0600

    comment entity created, no mappings or Junit yet

---
+++ JPARollTheDice/src/main/java/com/skilldistillery/rollthedice/entities/Comment.java
@@ -0,0 +1,63 @@
+package com.skilldistillery.rollthedice.entities;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Comment {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String message;
+
+	public Comment() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Comment other = (Comment) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "Comment [id=" + id + ", message=" + message + "]";
+	}
+	
+	
+
+}
