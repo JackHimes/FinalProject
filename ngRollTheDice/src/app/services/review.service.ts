90aaa48b010d55dadd7083994e8546187ce408b9
Andrew Shumway <shumway.andrew@yahoo.com>
2022-01-28 09:43:44 -0700

    Review/User services ready

--- ngRollTheDice/src/app/services/review.service.ts
+++ ngRollTheDice/src/app/services/review.service.ts
@@ -1,9 +1,85 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Review } from '../models/review';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReviewService {
 
-  constructor() { }
+  private url = environment.baseUrl + 'api/reviews'
+
+  constructor(private http: HttpClient, private auth: AuthService) { }
+
+  getHttpOptions() {
+    let options = {
+      headers: {
+        Authorization: 'Basic ' + this.auth.getCredentials(),
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+    };
+    return options;
+  }
+
+  index(): Observable<Review> {
+    return this.http.get<Review>(this.url, this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () =>
+            new Error('ReviewService.index(): error retreiving reviews: ' + err)
+        );
+      })
+    )
+  }
+
+  show(id: number): Observable<Review> {
+    return this.http.get<Review>(this.url + '/' + id, this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError (
+          () => new Error('ReviewService.show(): error retreiving review: ' + err)
+        );
+      })
+    );
+  }
+
+  update(review: Review, id: number): Observable<Review> {
+    return this.http.put<Review>(this.url + '/' + id, review, this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () => new Error('ReviewService.update(): error updating review: ' + err)
+        );
+      })
+    );
+  }
+
+  destroy(id: number): Observable<void> {
+    return this.http.delete<void>(this.url + '/' + id, this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () => new Error('ReviewService.destroy(): error destroying review: ' + err)
+        );
+      })
+    )
+  }
+
+  create(review: Review): Observable<Review> {
+    return this.http.post<Review>(this.url + '/review', review, this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () =>
+            new Error('ReviewService.create(): error creating review: ' + err)
+        );
+      })
+    )
+  }
+
+
 }
--- ngRollTheDice/src/app/services/user.service.ts
+++ ngRollTheDice/src/app/services/user.service.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { User } from '../models/user';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +12,73 @@ export class UserService {
 
   private url = environment.baseUrl + 'api/users';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private auth: AuthService) { }
+
+  getHttpOptions() {
+    let options = {
+      headers: {
+        Authorization: 'Basic ' + this.auth.getCredentials(),
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+    };
+    return options;
+  }
+
+  index(): Observable<User> {
+    return this.http.get<User>(this.url, this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () =>
+            new Error('UserService.index(): error retreiving users: ' + err)
+        );
+      })
+    )
+  }
+
+  show(id: number): Observable<User> {
+    return this.http.get<User>(this.url + '/' + id, this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError (
+          () => new Error('UserService.show(): error retreiving user: ' + err)
+        );
+      })
+    );
+  }
+
+  update(user: User, id: number): Observable<User> {
+    return this.http.put<User>(this.url + '/' + id, user, this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () => new Error('UserService.update(): error updating user: ' + err)
+        );
+      })
+    );
+  }
+
+  destroy(id: number): Observable<void> {
+    return this.http.delete<void>(this.url + '/' + id, this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () => new Error('UserService.destroy(): error destroying user: ' + err)
+        );
+      })
+    )
+  }
+
+  addGuestToGameEvent(user: User): Observable<User> {
+    return this.http.put<User>(this.url, user, this.getHttpOptions()).pipe(
+        catchError((err: any) => {
+          console.log(err);
+          return throwError(
+            () => new Error('UserService.addGuestToGameEvent(): error adding guest to GameEvent: ' + err)
+          );
+        })
+      );
+  }
+
+
 }
