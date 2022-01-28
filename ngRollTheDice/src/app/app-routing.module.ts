5e8d600449a2d3b24552a48c9dac8e5f1589812b
Ian Verderame <ianverderame9@gmail.com>
2022-01-28 10:42:38 -0700

    added routes in app-routing.module.ts

--- ngRollTheDice/src/app/app-routing.module.ts
+++ ngRollTheDice/src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdvancedsearchComponent } from './components/advancedsearch/advancedsearch.component';
+import { CreationComponent } from './components/creation/creation.component';
+import { GamedetailsComponent } from './components/gamedetails/gamedetails.component';
+import { GameeventdetailsComponent } from './components/gameeventdetails/gameeventdetails.component';
+import { HomeComponent } from './components/home/home.component';
 import { LoginregisterComponent } from './components/loginregister/loginregister.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 const routes: Routes = [
+  {path: 'advancedsearch', component: AdvancedsearchComponent},
+  {path: 'creation', component: CreationComponent},
+  {path: 'gamedetails', component: GamedetailsComponent},
+  {path: 'gameeventdetails', component: GameeventdetailsComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'navigation', component: NavigationComponent},
+  {path: 'profile', component: ProfileComponent},
   {path: 'loginregister', component: LoginregisterComponent}
 ];
 
