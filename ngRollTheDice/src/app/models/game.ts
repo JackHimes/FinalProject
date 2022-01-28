a96bb1c96ad3afcb4efc2a4ee85a6515b6cd0962
Ian Verderame <ianverderame9@gmail.com>
2022-01-28 09:55:39 -0700

    game model and service with CRUD

--- ngRollTheDice/src/app/models/game.ts
+++ ngRollTheDice/src/app/models/game.ts
@@ -1,2 +1,31 @@
+import { Gameevent } from "./gameevent";
+import { User } from "./user";
+
 export class Game {
+
+id: number | undefined;
+name: string | undefined;
+maxPlayers: number | undefined;
+linkToGame: string | undefined;
+timeToPlay: string | undefined;
+description: string | undefined;
+imageUrl: string | undefined;
+users: User[] | undefined;
+gameEvents: Gameevent[] | undefined;
+gameOwner: User | undefined;
+
+constructor(id?: number, name?: string, maxPlayers?: number, linkToGame?: string, timeToPlay?: string, description?: string, imageUrl?: string,
+  users?: User[], gameEvents?: Gameevent[], gameOwner?: User) {
+  this.id = id;
+  this.name = name;
+  this.maxPlayers = maxPlayers;
+  this.linkToGame = linkToGame;
+  this.timeToPlay = timeToPlay;
+  this.description = description;
+  this.imageUrl = imageUrl;
+  this.users = users;
+  this.gameEvents = gameEvents;
+  this.gameOwner = gameOwner;
+}
+
 }
---
+++ ngRollTheDice/src/app/services/game.service.ts
@@ -0,0 +1,86 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Game } from '../models/game';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GameService {
+  private url = environment.baseUrl +'api/games';
+
+  constructor(
+    private http: HttpClient,
+    private auth: AuthService
+  ) { }
+
+getHttpOptions() {
+  let options = {
+    headers: {
+      Authorization: 'Basic ' + this.auth.getCredentials(),
+      'X-Requested-With': 'XMLHttpRequest',
+    },
+  };
+  return options;
+}
+
+show(id: number): Observable<Game> {
+  return this.http.get<Game>(this.url + '/' + id, this.getHttpOptions()).pipe(
+    catchError((err: any) => {
+      console.log(err);
+      return throwError (
+        () => new Error('GameService.show(): error retreiving game: ' + err)
+      );
+    })
+  );
+}
+
+index(): Observable<Game> {
+  return this.http.get<Game>(this.url, this.getHttpOptions()).pipe(
+    catchError((err: any) => {
+      console.log(err);
+      return throwError(
+        () =>
+          new Error('GameSvc.index(): error retreiving games: ' + err)
+      );
+    })
+  )
+}
+
+create(game: Game): Observable<Game> {
+  return this.http.post<Game>(this.url, game, this.getHttpOptions()).pipe(
+    catchError((err: any) => {
+      console.log(err);
+      return throwError(
+        () =>
+          new Error('GameService.create(): error creating game: ' + err)
+      );
+    })
+  )
+}
+
+update(game: Game): Observable<Game> {
+  return this.http.put<Game>(this.url + `/${game.id}`, game, this.getHttpOptions()).pipe(
+    catchError((err: any) => {
+      console.log(err);
+      return throwError(
+        () => new Error('GameService.update(): error updating game: ' + err)
+      );
+    })
+  );
+}
+
+destroy(gameId: number): Observable<void> {
+  return this.http.delete<void>(this.url + '/' + gameId, this.getHttpOptions()).pipe(
+    catchError((err: any) => {
+      console.log(err);
+      return throwError(
+        () => new Error('GameService.destroy(): error destroying game: ' + err)
+      );
+    })
+  )
+}
+
+}
