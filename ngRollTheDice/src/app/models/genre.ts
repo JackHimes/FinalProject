cb3265dad967bad54c175052011df5018303e95a
Max McGregor <maxmcgregor1@gmail.com>
2022-01-28 10:36:51 -0600

    genre model/service complete in vs code

--- ngRollTheDice/src/app/models/genre.ts
+++ ngRollTheDice/src/app/models/genre.ts
@@ -1,2 +1,21 @@
+import { Game } from "./game";
+
 export class Genre {
+  id: number;
+  name: string | undefined;
+  description: string | undefined;
+  games: Game[] | undefined;
+
+  constructor(
+    id: number = 0,
+    name: string,
+    description: string,
+    games: Game[]
+  ){
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.games = games;
+  }
+
 }
---
+++ ngRollTheDice/src/app/services/genre.service.ts
@@ -0,0 +1,75 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Genre } from '../models/genre';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GenreService {
+  private url = environment.baseUrl +'genres';
+
+
+  constructor(
+  private http: HttpClient,
+  private auth: AuthService
+  ) { }
+
+getHttpOptions() {
+ let options = {
+    headers: {
+      Authorization: 'Basic ' + this.auth.getCredentials(),
+      'X-Requested-With': 'XMLHttpRequest',
+    },
+  };
+  return options;
+}
+
+show(id: number): Observable<Genre> {
+  return this.http.get<Genre>(this.url + '/' + id, this.getHttpOptions()).pipe(
+    catchError((err: any) => {
+      console.log(err);
+      return throwError (
+        () => new Error('GenreService.show(): error retreiving genre: ' + err)
+      );
+    })
+  );
+}
+
+index(): Observable<Genre> {
+  return this.http.get<Genre>(this.url, this.getHttpOptions()).pipe(
+    catchError((err: any) => {
+      console.log(err);
+      return throwError(
+        () =>
+          new Error('GenreService.index(): error retreiving genre: ' + err)
+      );
+    })
+  )
+}
+
+update(genre: Genre, id: number): Observable<Genre> {
+  return this.http.put<Genre>(this.url + '/' + id, genre, this.getHttpOptions()).pipe(
+    catchError((err: any) => {
+      console.log(err);
+      return throwError(
+        () => new Error('GenreService.create(): error updating genre: ' + err)
+      );
+    })
+  );
+}
+
+destroy(id: number): Observable<void> {
+  return this.http.delete<void>(this.url + '/' + id, this.getHttpOptions()).pipe(
+    catchError((err: any) => {
+      console.log(err);
+      return throwError(
+        () => new Error('GenreService.create(): error destroying genre: ' + err)
+      );
+    })
+  )
+}
+
+}
